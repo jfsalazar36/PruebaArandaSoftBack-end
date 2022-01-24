f17396f38596bf8fc6260bc2e7b3f5d700cb97b4
jfsalazar36 <52377293+jfsalazar36@users.noreply.github.com>
2022-01-23 21:23:06 -0500

    Update script_BD_ArandaSoft.sql
    Script que contiene la base de datos de la prueba ArandaSoft.

--- script_BD_ArandaSoft.sql
+++ script_BD_ArandaSoft.sql
@@ -1,113 +1,148 @@
-USE [master]
+USE [BD_ArandaSoft]
 GO
-
-/****** Object:  Database [BD_ArandaSoft]    Script Date: 23/01/2022 9:11:58 p. m. ******/
-CREATE DATABASE [BD_ArandaSoft]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'BD_ArandaSoft', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLSERVER2019\MSSQL\DATA\BD_ArandaSoft.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'BD_ArandaSoft_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLSERVER2019\MSSQL\DATA\BD_ArandaSoft_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
- WITH CATALOG_COLLATION = DATABASE_DEFAULT
+/****** Object:  Table [dbo].[Permisos]    Script Date: 23/01/2022 9:18:30 p. m. ******/
+SET ANSI_NULLS ON
 GO
-
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [BD_ArandaSoft].[dbo].[sp_fulltext_database] @action = 'enable'
-end
+SET QUOTED_IDENTIFIER ON
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET ANSI_NULL_DEFAULT OFF 
+CREATE TABLE [dbo].[Permisos](
+	[PermisoId] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](80) NOT NULL,
+ CONSTRAINT [PK_Permisos] PRIMARY KEY CLUSTERED 
+(
+	[PermisoId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET ANSI_NULLS OFF 
+/****** Object:  Table [dbo].[PermisosPorRol]    Script Date: 23/01/2022 9:18:30 p. m. ******/
+SET ANSI_NULLS ON
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET ANSI_PADDING OFF 
+SET QUOTED_IDENTIFIER ON
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET ANSI_WARNINGS OFF 
+CREATE TABLE [dbo].[PermisosPorRol](
+	[PermisoPorRolId] [int] IDENTITY(1,1) NOT NULL,
+	[RolId] [int] NOT NULL,
+	[PermisoId] [int] NOT NULL,
+ CONSTRAINT [PK_PermisosPorRol] PRIMARY KEY CLUSTERED 
+(
+	[PermisoPorRolId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET ARITHABORT OFF 
+/****** Object:  Table [dbo].[Roles]    Script Date: 23/01/2022 9:18:30 p. m. ******/
+SET ANSI_NULLS ON
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET AUTO_CLOSE OFF 
+SET QUOTED_IDENTIFIER ON
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET AUTO_SHRINK OFF 
+CREATE TABLE [dbo].[Roles](
+	[RolId] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](20) NOT NULL,
+ CONSTRAINT [PK_Roles] PRIMARY KEY CLUSTERED 
+(
+	[RolId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET AUTO_UPDATE_STATISTICS ON 
+/****** Object:  Table [dbo].[RolesPorUsuario]    Script Date: 23/01/2022 9:18:30 p. m. ******/
+SET ANSI_NULLS ON
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET CURSOR_CLOSE_ON_COMMIT OFF 
+SET QUOTED_IDENTIFIER ON
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET CURSOR_DEFAULT  GLOBAL 
+CREATE TABLE [dbo].[RolesPorUsuario](
+	[RolPorUsuarioId] [int] IDENTITY(1,1) NOT NULL,
+	[UsuarioId] [int] NOT NULL,
+	[RolId] [int] NOT NULL,
+ CONSTRAINT [PK_RolesPorUsuario] PRIMARY KEY CLUSTERED 
+(
+	[RolPorUsuarioId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET CONCAT_NULL_YIELDS_NULL OFF 
+/****** Object:  Table [dbo].[Usuarios]    Script Date: 23/01/2022 9:18:30 p. m. ******/
+SET ANSI_NULLS ON
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET NUMERIC_ROUNDABORT OFF 
+SET QUOTED_IDENTIFIER ON
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET QUOTED_IDENTIFIER OFF 
+CREATE TABLE [dbo].[Usuarios](
+	[UsuarioId] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](30) NULL,
+	[Password] [varchar](20) NULL,
+	[PrimerNombre] [varchar](80) NULL,
+	[SegundoNombre] [varchar](80) NULL,
+	[PrimerApellido] [varchar](80) NULL,
+	[SegundoApellido] [varchar](80) NULL,
+	[Direccion] [varchar](120) NULL,
+	[Telefono] [varchar](12) NULL,
+	[Email] [varchar](50) NULL,
+	[Edad] [int] NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[UsuarioId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
+SET IDENTITY_INSERT [dbo].[Permisos] ON 
 
-ALTER DATABASE [BD_ArandaSoft] SET RECURSIVE_TRIGGERS OFF 
+INSERT [dbo].[Permisos] ([PermisoId], [Nombre]) VALUES (1, N'MostrarMensaje')
+INSERT [dbo].[Permisos] ([PermisoId], [Nombre]) VALUES (2, N'MostrarNoticias')
+INSERT [dbo].[Permisos] ([PermisoId], [Nombre]) VALUES (3, N'Consultar')
+INSERT [dbo].[Permisos] ([PermisoId], [Nombre]) VALUES (4, N'Editar')
+INSERT [dbo].[Permisos] ([PermisoId], [Nombre]) VALUES (5, N'Crear')
+INSERT [dbo].[Permisos] ([PermisoId], [Nombre]) VALUES (6, N'Eliminar')
+SET IDENTITY_INSERT [dbo].[Permisos] OFF
+SET IDENTITY_INSERT [dbo].[PermisosPorRol] ON 
+
+INSERT [dbo].[PermisosPorRol] ([PermisoPorRolId], [RolId], [PermisoId]) VALUES (1, 4, 1)
+INSERT [dbo].[PermisosPorRol] ([PermisoPorRolId], [RolId], [PermisoId]) VALUES (2, 4, 2)
+INSERT [dbo].[PermisosPorRol] ([PermisoPorRolId], [RolId], [PermisoId]) VALUES (3, 4, 3)
+INSERT [dbo].[PermisosPorRol] ([PermisoPorRolId], [RolId], [PermisoId]) VALUES (4, 4, 4)
+INSERT [dbo].[PermisosPorRol] ([PermisoPorRolId], [RolId], [PermisoId]) VALUES (5, 4, 5)
+INSERT [dbo].[PermisosPorRol] ([PermisoPorRolId], [RolId], [PermisoId]) VALUES (6, 4, 6)
+INSERT [dbo].[PermisosPorRol] ([PermisoPorRolId], [RolId], [PermisoId]) VALUES (7, 1, 1)
+INSERT [dbo].[PermisosPorRol] ([PermisoPorRolId], [RolId], [PermisoId]) VALUES (8, 1, 2)
+INSERT [dbo].[PermisosPorRol] ([PermisoPorRolId], [RolId], [PermisoId]) VALUES (9, 2, 1)
+INSERT [dbo].[PermisosPorRol] ([PermisoPorRolId], [RolId], [PermisoId]) VALUES (10, 2, 3)
+INSERT [dbo].[PermisosPorRol] ([PermisoPorRolId], [RolId], [PermisoId]) VALUES (11, 3, 1)
+INSERT [dbo].[PermisosPorRol] ([PermisoPorRolId], [RolId], [PermisoId]) VALUES (12, 3, 3)
+INSERT [dbo].[PermisosPorRol] ([PermisoPorRolId], [RolId], [PermisoId]) VALUES (13, 3, 4)
+SET IDENTITY_INSERT [dbo].[PermisosPorRol] OFF
+SET IDENTITY_INSERT [dbo].[Roles] ON 
+
+INSERT [dbo].[Roles] ([RolId], [Nombre]) VALUES (1, N'Visitante')
+INSERT [dbo].[Roles] ([RolId], [Nombre]) VALUES (2, N'Asistente')
+INSERT [dbo].[Roles] ([RolId], [Nombre]) VALUES (3, N'Editor')
+INSERT [dbo].[Roles] ([RolId], [Nombre]) VALUES (4, N'Administrador')
+SET IDENTITY_INSERT [dbo].[Roles] OFF
+SET IDENTITY_INSERT [dbo].[RolesPorUsuario] ON 
+
+INSERT [dbo].[RolesPorUsuario] ([RolPorUsuarioId], [UsuarioId], [RolId]) VALUES (1, 1, 4)
+SET IDENTITY_INSERT [dbo].[RolesPorUsuario] OFF
+SET IDENTITY_INSERT [dbo].[Usuarios] ON 
+
+INSERT [dbo].[Usuarios] ([UsuarioId], [Nombre], [Password], [PrimerNombre], [SegundoNombre], [PrimerApellido], [SegundoApellido], [Direccion], [Telefono], [Email], [Edad]) VALUES (1, N'admon', N'123', N'Administrador', NULL, N'ArandaSoft', NULL, NULL, NULL, N'admin@arandasoft.com', NULL)
+SET IDENTITY_INSERT [dbo].[Usuarios] OFF
+ALTER TABLE [dbo].[PermisosPorRol]  WITH CHECK ADD  CONSTRAINT [FK_PermisosPorRol_Permisos] FOREIGN KEY([PermisoId])
+REFERENCES [dbo].[Permisos] ([PermisoId])
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET  DISABLE_BROKER 
+ALTER TABLE [dbo].[PermisosPorRol] CHECK CONSTRAINT [FK_PermisosPorRol_Permisos]
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+ALTER TABLE [dbo].[PermisosPorRol]  WITH CHECK ADD  CONSTRAINT [FK_PermisosPorRol_Roles] FOREIGN KEY([RolId])
+REFERENCES [dbo].[Roles] ([RolId])
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET DATE_CORRELATION_OPTIMIZATION OFF 
+ALTER TABLE [dbo].[PermisosPorRol] CHECK CONSTRAINT [FK_PermisosPorRol_Roles]
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET TRUSTWORTHY OFF 
+ALTER TABLE [dbo].[RolesPorUsuario]  WITH CHECK ADD  CONSTRAINT [FK_RolesPorUsuario_Roles] FOREIGN KEY([RolId])
+REFERENCES [dbo].[Roles] ([RolId])
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+ALTER TABLE [dbo].[RolesPorUsuario] CHECK CONSTRAINT [FK_RolesPorUsuario_Roles]
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET PARAMETERIZATION SIMPLE 
+ALTER TABLE [dbo].[RolesPorUsuario]  WITH CHECK ADD  CONSTRAINT [FK_RolesPorUsuario_Usuarios] FOREIGN KEY([UsuarioId])
+REFERENCES [dbo].[Usuarios] ([UsuarioId])
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET READ_COMMITTED_SNAPSHOT OFF 
+ALTER TABLE [dbo].[RolesPorUsuario] CHECK CONSTRAINT [FK_RolesPorUsuario_Usuarios]
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET HONOR_BROKER_PRIORITY OFF 
+ALTER TABLE [dbo].[Usuarios]  WITH CHECK ADD  CONSTRAINT [FK_Usuarios_Usuarios] FOREIGN KEY([UsuarioId])
+REFERENCES [dbo].[Usuarios] ([UsuarioId])
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET RECOVERY FULL 
+ALTER TABLE [dbo].[Usuarios] CHECK CONSTRAINT [FK_Usuarios_Usuarios]
 GO
-
-ALTER DATABASE [BD_ArandaSoft] SET  MULTI_USER 
-GO
-
-ALTER DATABASE [BD_ArandaSoft] SET PAGE_VERIFY CHECKSUM  
-GO
-
-ALTER DATABASE [BD_ArandaSoft] SET DB_CHAINING OFF 
-GO
-
-ALTER DATABASE [BD_ArandaSoft] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
-GO
-
-ALTER DATABASE [BD_ArandaSoft] SET TARGET_RECOVERY_TIME = 60 SECONDS 
-GO
-
-ALTER DATABASE [BD_ArandaSoft] SET DELAYED_DURABILITY = DISABLED 
-GO
-
-ALTER DATABASE [BD_ArandaSoft] SET QUERY_STORE = OFF
-GO
-
-ALTER DATABASE [BD_ArandaSoft] SET  READ_WRITE 
-GO
-
-
